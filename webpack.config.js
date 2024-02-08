f3edcd1735bdadb64e7a3322487ac68f1c19400a
sbpark88 <saebyulpark88@gmail.com>
2024-02-09 00:53:54 +0900

    Chore: Add file-loader, reset-css libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "assets/resource",
+        generator: {
+          filename: "assets/images/[hash][ext][query]", // 이미지가 번들될 위치를 변경
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html' // 루트 디렉토리에 있는 index.html을 템플릿으로 사용
-    })
-  ]
+      template: "index.html", // 루트 디렉토리에 있는 index.html을 템플릿으로 사용
+    }),
+  ],
 };
